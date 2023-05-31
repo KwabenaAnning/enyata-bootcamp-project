6a4c2b34d83584576fd8d04db85e0b8eea9cbac2
Ghazi Anning <sammy.khobbie>
2023-05-31 23:54:03 +0000

    enyata-bootcamp-project

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const form =document.querySelector{form}
+
+const formName =document.querySelector{#Name}
+
+const formEmail =document.querySelector{#Email}
+
+const error =document.querySelector{#error}
