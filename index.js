1d125c3c676779ebae85c4e10c1c46546e87fa6d
Ghazi Anning <sammy.khobbie>
2023-06-01 08:51:49 +0000

    enyata-bootcamp-project

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 
-const form =document.querySelector{form}
+const form =document.querySelector('form')
 
-const formName =document.querySelector{#Name}
+const formName =document.querySelector('#Name')
 
-const formEmail =document.querySelector{#Email}
+const formEmail =document.querySelector('#Email')
 
-const error =document.querySelector{#error}
+const error =document.querySelector('#error')
+
+form.addeventlistener('submit', function(e){e.preventDefault()
+
+    if(formName.value === ''||form.value ===''){error.getElementsByClassName.display = 'block'
+
+    setTimeout(()=>{
+        error.getElementsByClassName.display = 'none'
+    },5000)
+
+    return
+}
+
+alert(`Hello ${formName.value},your form has been submitted successfully.\nYour email is ${formEmail.value}`)
+   console.log(formName.value)
+   console.log(formEmail.value)
+
+ formName.value = ''
+ formEmail.value = ''
+})
